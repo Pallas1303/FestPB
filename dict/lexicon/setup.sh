666a4b3d1eaf3b0b206790124ec285b7840dad5b
Pallas Da Silva Guedes <103946076+Pallas1303@users.noreply.github.com>
2024-04-29 20:49:55 -0300

    Correção de download de dicionário fonético feito pelo Falabrasil.
    Foi resolvido o problema presente na linha 11. O download usando wget não estava sendo feito, o link antigo estava redirecionado para uma pagina inexistente. 
    
    Eu atualizei o link para um link corrento.

--- dict/lexicon/setup.sh
+++ dict/lexicon/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Baixar dicionário fonétiFã original feito pelo Falabrasil.
+#Baixar dicionário fonético feito pelo Falabrasil.
 
 if [ -e lexicon.utf8.dict.gz ]
 then
@@ -8,15 +8,14 @@ gzip -d lexicon.utf8.dict.gz
 else
 if [ ! -e lexicon.utf8.dict ]
 then
-wget https://gitlab.com/fb-nlp/nlp-resources/-/raw/master/res/lexicon.utf8.dic.gz
+wget https://gitlab.com/fb-nlp/nlp-resources/-/raw/master/res/lexicon.utf8.dict.gz?ref_type=heads&inline=false
 gzip -d lexicon.utf8.dic.gz
 fi
 fi
+
 echo "Deseja converter o dicionário do Falabrasil para um novo dicionário compatível com Festival Speech Synthesis?"
 echo "s para sim e n para não"
 read resposta
-
-
 
 if [ $resposta = "s" ]
 then
